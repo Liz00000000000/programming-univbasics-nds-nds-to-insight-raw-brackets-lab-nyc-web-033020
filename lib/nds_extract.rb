32bec3988d692314f23245d267d2839a86b43ff6
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-28 23:06:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,6 +9,8 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
+
+  result[nds[:movies][:director]] = nds[:movies][:worldwide_gross]
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
