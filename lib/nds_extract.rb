d11d63e0618d1060cb4afca3107817c4b20c413e
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-28 23:09:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,8 +11,8 @@ def directors_totals(nds)
   }
 
   nds[:movie][:director].each do |director|
-    result[director] << nds[:movie][:worldwide_gross]
-  end 
+    result[director] = nil 
+  end
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
